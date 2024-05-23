0b7a81498e8e5736bb9e14ab9f300881407e9e71
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-05-24 01:31:36 +0300

    Add files via upload

---
+++ 01.ReadText/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.ReadText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Stop")
+                {
+                    break;
+                }
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
